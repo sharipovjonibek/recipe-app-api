5211452367d41c7ca19bab0f8dfb13667877fd19
sharipovjonibek <jsharipov559@gmail.com>
2025-09-08 03:10:28 +0500

    Fixed some errors.

--- app/app/tests.py
+++ app/app/tests.py
@@ -5,15 +5,18 @@ Sample tests
 
 from django.test import SimpleTestCase
 
+
 from app import calc
+
 
 class CalcTests(SimpleTestCase):
     """ Test the calc module."""
 
+
     def test_add_numbers(self):
         """ Test adding numbers together. """
 
-        res=calc.add(5, 6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
@@ -23,4 +26,3 @@ class CalcTests(SimpleTestCase):
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
-
